b05f5933c3cb22410e1dfb2ede4b886ca8bb9ec0
Ali Karbassi <ali@karbassi.com>
2012-11-05 15:09:11 -0600

    Changes source to allow http:// or https://

--- youtube.ga.js
+++ youtube.ga.js
@@ -6,7 +6,7 @@
 
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
+tag.src = "//youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
